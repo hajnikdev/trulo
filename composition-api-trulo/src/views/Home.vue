6d984292fd3e06a89694fe5e64c6ade3b5b7584f
Maroš Hajník <hajnik.work@gmail.com>
2021-04-21 21:04:23 +0200

    Project cleanup

--- composition-api-trulo/src/views/Home.vue
+++ composition-api-trulo/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <EmptyTemplate />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EmptyTemplate from '@/components/EmptyTemplate.vue'
 
 export default {
-  name: "Home",
+  name: 'EmptyTemplate',
   components: {
-    HelloWorld,
+    EmptyTemplate,
   },
-};
+}
 </script>
