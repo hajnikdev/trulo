39b679bb39346f9ce4ee83d5e3780d5cabbaeb26
Maroš Hajník <hajnik.work@gmail.com>
2021-04-21 21:43:56 +0200

    onMounted life cycle hook, simple example of this method.

--- composition-api-trulo/src/views/Home.vue
+++ composition-api-trulo/src/views/Home.vue
@@ -6,13 +6,17 @@
 </template>
 
 <script>
-import { reactive, toRefs } from 'vue'
+import { reactive, toRefs, onMounted } from 'vue'
 
 export default {
   setup() {
     const state = reactive({
       score: 0,
       dude: 'Ahoj',
+    })
+
+    onMounted(() => {
+      state.score = 100
     })
 
     return {
