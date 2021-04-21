893e8b87e2c20b50d0c86e5d5af73784dc6a7a9f
Maroš Hajník <hajnik.work@gmail.com>
2021-04-21 21:26:33 +0200

    Components creation using composition Api (ref + computed example)

--- composition-api-trulo/src/views/Home.vue
+++ composition-api-trulo/src/views/Home.vue
@@ -1,17 +1,26 @@
 <template>
   <div>
-    <EmptyTemplate />
+    <button @click="score++">{{ score }} score double = {{ double }}</button>
   </div>
+  <p>{{ hello }}</p>
 </template>
 
 <script>
-// @ is an alias to /src
-import EmptyTemplate from '@/components/EmptyTemplate.vue'
+import { ref, computed } from 'vue'
 
 export default {
-  name: 'EmptyTemplate',
-  components: {
-    EmptyTemplate,
+  setup() {
+    /**  vyhoda zgrupovania casti kodu teda vlastna organizacia kodu*/
+
+    // data
+    const score = ref(0)
+    const dude = ref('Bulma')
+
+    // computed (v3computed)
+    const double = computed(() => score.value * 2)
+    const hello = computed(() => 'Hello, I am ' + dude.value)
+
+    return { score, double, hello }
   },
 }
 </script>
